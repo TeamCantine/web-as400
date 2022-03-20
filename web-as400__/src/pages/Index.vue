daaaba67c51684c90ca676f9ae05fd0a3c8812a2
TeamCantine <andrea.vicari@sanmarcoinformatica.it>
2022-03-20 23:02:02 +0100

    c

--- web-as400__/src/pages/Index.vue
+++ web-as400__/src/pages/Index.vue
@@ -1,16 +1,65 @@
 <template>
+  <div class="q-pa-md">
   <h3>Tomas table</h3>
 
-  <div class="q-pa-md">
+
+
+  <q-select
+        filled
+        v-model="model"
+        use-input
+        input-debounce="0"
+        label="Simple filter"
+        :options="options"
+        @filter="filterFn"
+        style="width: 250px"
+        behavior="menu"
+      >
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              No results
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+
+
+  <q-toggle v-model="grid" label="Grid" class="q-mb-md" />
     <q-table
+    class="my-sticky-header-table"
       dense
+      :grid="grid"
       :rows="rows"
       :columns="columns"
-      :rows-per-page-options="[0, 8, 18]"
+      row-key="name"
+      :loading="loading"
       boarderd
       :title="fileName"
-      row-key="indexs"
-    />
+      separator="cell"
+      :rowsPerPage="30"
+      :rows-per-page-options="[0, 8, 18]"
+      style="height: 500px"
+      :table-header-style="{ backgroundColor: '#ff0000' }"
+      :filter="filter"
+    >
+
+
+
+    <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+
+
+
+
+    </q-table>
+
   </div>
 </template>
 
@@ -18,20 +67,22 @@
 import { ref } from "vue";
 
 export default {
+
   data() {
     return {
+      model: ref(null),
+      stringOptions: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ],
+      options:  this.stringOptions,
       separator: ref("vertical"),
+      filter: ref(''),
+      grid:ref(false),
       loading: false,
       fileName: "ROLE_USER",
       lib: "WRKJEXP",
       rows: [],
-      indexs:[],
       columns: [
-        {
-          name: "index",
-          label: "#",
-          field: "indexs",
-        },
         {
           name: "COLUMN_NAME",
           label: "CAMPO",
@@ -72,9 +123,29 @@ export default {
     };
   },
   methods: {
+
+
+    filterFn (val, update) {
+        if (val === '') {
+          update(() => {
+            this.options = this.stringOptions
+            console.log(this.model)
+          })
+          return
+        }
+
+        update(() => {
+          const needle = val.toLowerCase()
+          this.options = this.stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+          console.log(this.options)
+
+        })
+      },
+
+
+
     async loadFiles() {
       this.loading = true;
-      console.log(this.tableName);
       try {
         const data = {
           lib: this.lib,
@@ -83,18 +154,41 @@ export default {
         await this.$store.dispatch("files/getFilesAction", data);
         this.rows = this.$store.getters["files/getFilesGetter"];
         this.loading = false;
-
-        this.rows.forEach((row, index) => {
-          this.indexs[index] = index
-        });
       } catch (error) {
         console.log(error);
         this.loading = false;
       }
     },
+
   },
   created() {
     this.loadFiles();
   },
 };
 </script>
+
+
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 310px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #eeeeee
+    color: #673BB6
+    height: 40px
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+</style>
